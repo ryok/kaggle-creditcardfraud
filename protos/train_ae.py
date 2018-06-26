bcd14171350f921eed727caa94644042b9920b6b
Ryo Okada <atribecalledquest448@gmail.com>
2018-06-26 18:50:20 +0900

    edit train_ae.py and train_nn.py

--- protos/train_ae.py
+++ protos/train_ae.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from scipy import stats
 import tensorflow as tf
+import seaborn as sns
+from pylab import rcParams
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.utils.np_utils import to_categorical
+from keras.models import Model, load_model
+from keras.layers import Input, Dense
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras import regularizers
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('../input/creditcard.csv')
-#count_classes = pd.DataFrame(pd.value_counts(df['Class'], sort = True).sort_index())
-#print(count_classes)
 
-train, test = train_test_split(df, test_size=0.2, random_state=0, stratify=df['Class'])
+X_train, X_test = train_test_split(df, test_size=0.2, random_state=2)
+# semi supervised training
+X_train = X_train[X_train.Class == 0]
+X_train = X_train.drop(['Class'], axis=1)
+Y_train = X_train['Class']
+X_test = X_test.drop(['Class'], axis=1)
+X_train = X_train.values
+X_test = X_test.values
 
-X_train = train.drop(['Class'], axis=1)
-X_test = test.drop(['Class'], axis=1)
-y_train = train['Class']
-y_test = test['Class']
 
-print('X_train: {}'.format(X_train.shape))
-print('y_train: {}'.format(y_train.shape))
-print('X_test: {}'.format(X_test.shape))
-print('y_test: {}'.format(y_test.shape))
--- protos/train_nn.py
+++ protos/train_nn.py
@@ -1,6 +1,66 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.utils.np_utils import to_categorical
 
-df = pd.read_csv("../input/creditcard.csv")
+df = pd.read_csv('../input/creditcard.csv')
 
-df.head()
+train, test = train_test_split(df, test_size=0.2, random_state=0, stratify=df['Class'])
+
+X_train = train.drop(['Class'], axis=1)
+X_test = test.drop(['Class'], axis=1)
+Y_train = train['Class']
+Y_test = test['Class']
+
+print('X_train: {}'.format(X_train.shape))
+print('Y_train: {}'.format(Y_train.shape))
+print('X_test: {}'.format(X_test.shape))
+print('Y_test: {}'.format(Y_test.shape))
+
+print(Y_train.head())
+
+Y_train = to_categorical(Y_train, num_classes=2)
+Y_test = to_categorical(Y_test, num_classes=2)
+
+np.random.seed(0)
+
+model = Sequential()
+model.add(Dense(64, input_dim=30, activation='relu'))
+model.add(Dropout(0.9))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.9))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.9))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.9))
+model.add(Dense(2, activation='softmax'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# training and test
+epoch = 10
+batch_size = 2048
+model.fit(X_train, Y_train, epochs=epoch, batch_size=batch_size)
+
+score, acc = model.evaluate(X_test, Y_test)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+history = model.fit(X_train, Y_train, epochs=20, batch_size=batch_size, validation_data=(X_test, Y_test), verbose=2)
+history.history.keys()
+
+fig, ax = plt.subplots(2,1)
+ax[0].plot(history.history['loss'], color='b', label="Training loss")
+ax[0].plot(history.history['val_loss'], color='r', label="Testing loss",axes =ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
+ax[1].plot(history.history['val_acc'], color='r',label="Testing accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
+plt.show()
+
